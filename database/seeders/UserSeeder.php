30e60338f0ab71f09f89ab69690bd36533ebc59f
Manukyan <astghik.manukyan@lycee-merleauponty.fr>
2025-05-07 10:54:02 +0200

    user fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +18,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'test@email.com',
+            'is_garage' => true,
+            'password' => Hash::make('admin'), // password
+            'remember_token' => Str::random(10),
+            ]);
         // Use the User factory to create 10 users
         // You can adjust the number of users as needed
         User::factory(10)->create();
